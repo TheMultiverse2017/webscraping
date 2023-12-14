dd362ea57eeaec330cfbc0f3ecbc9fb1bc1d9d08
vishnuchandrancp360 <vishnu.chandran@cp-360.com>
2023-12-14 08:35:28 +0530

    Update RegisterController.php

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Auth;
 
+use App\Enum\RoleType;
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use App\Models\User;
@@ -66,6 +67,7 @@ class RegisterController extends Controller
     {
         return User::create([
             'name' => $data['name'],
+            'role' => RoleType::ADMIN->value,
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
         ]);
